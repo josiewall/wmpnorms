531de930b5806c444b9ca7d727d3e1037e9d1d1f
WMP\jwallner <jwallner@westmonroepartners.com>
2016-09-16 12:00:21 -0500

    Added new WMP norm on 9/16

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(function() {
+﻿$(function() {
   printRandomNorm();
 
   $("a#generate-new").click(function() {
@@ -12,10 +12,10 @@ var norms = [
   'We don’t let clients see us sweat.',
   'We are resourceful – it’s okay if we don’t know the answer, we’ll find it expeditiously and get back to our client.',
   'We frequently practice the “Around the Horn” tradition.',
-  'A sense of humor is a job requirement.'
+  'A sense of humor is a job requirement.',
+  'We always eat at least one pizza before noon.'
 ];
 
 function printRandomNorm() {
   var normInd = Math.floor(Math.random() * norms.length);
   $('p.norm').text(norms[normInd]);
-}
